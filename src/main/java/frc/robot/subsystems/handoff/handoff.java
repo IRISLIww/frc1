7f3d946bd34253d07b4e7230c71dcbdcc4aab6b7
IRISLIww <irissslii00@gmail.com>
2024-09-20 19:13:18 -0700

    hello

--- src/main/java/frc/robot/subsystems/handoff/handoff.java
+++ src/main/java/frc/robot/subsystems/handoff/handoff.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems.intake;
+package frc.robot.subsystems.handoff;
 
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
@@ -9,15 +9,15 @@ import com.ctre.phoenix6.signals.InvertedValue;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-public class intake extends SubsystemBase{
-    private final TalonFX intakemotor = new TalonFX(15, "canivore");
+public class handoff extends SubsystemBase{
+    private final TalonFX intakemotor = new TalonFX(10, "canivore");
     private TalonFXConfiguration intakeconfigs = new TalonFXConfiguration();
     private VoltageOut voltageRequest = new VoltageOut(0).withEnableFOC(true);
     private final StatusSignal<Double> current = intakemotor.getStatorCurrent();
     private final StatusSignal<Double> temp = intakemotor.getDeviceTemp();
     private final StatusSignal<Double> RPS = intakemotor.getRotorVelocity();
     private double setpointvolts =0.0; 
-    public intake() {
+    public handoff() {
        intakeconfigs.CurrentLimits.StatorCurrentLimit = 70;
        intakeconfigs.CurrentLimits.StatorCurrentLimitEnable = true;
        intakeconfigs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
@@ -33,7 +33,7 @@ public class intake extends SubsystemBase{
     @Override
     public void periodic(){
         BaseStatusSignal.refreshAll(current,temp,RPS);
-        SmartDashboard.putNumber("temperature", temp.getValue());
+        SmartDashboard.putNumber("temperature", temp.getVal7ue());
         SmartDashboard.putNumber("current", current.getValue());
         SmartDashboard.putNumber("RPS", RPS.getValue());
         SmartDashboard.putNumber("setpointvolts", setpointvolts);
